53431c83424e567782398f1b84305a4ff509be30
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-27 15:00:27 +0300

    Child and parent components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
     title = 'Hello world!';
-
-    intervalSub: any;
-
-    public ngOnInit(): void {
-        this.intervalSub = setInterval(() => {
-            console.log('Hello from hell!');
-        }, 1000);
-    }
-
-    public ngOnDestroy(): void {
-        if (this.intervalSub) {
-            clearInterval(this.intervalSub);
-        }
-    }
-
-    public getMin(a: number, b: number): number {
-        return a < b ? a : b;
-    }
 }
