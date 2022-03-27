e987d7eb58b872c6ced0b042c783129cafd8d46d
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-27 14:45:38 +0300

    Components templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,23 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
+    title = 'Hello world!';
+
     intervalSub: any;
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
         this.intervalSub = setInterval(() => {
             console.log('Hello from hell!');
         }, 1000);
     }
 
-    ngOnDestroy(): void {
+    public ngOnDestroy(): void {
         if (this.intervalSub) {
             clearInterval(this.intervalSub);
         }
+    }
+
+    public getMin(a: number, b: number): number {
+        return a < b ? a : b;
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HelloWorldComponent } from "./hello-world.component";
-
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
