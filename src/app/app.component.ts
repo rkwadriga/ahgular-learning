0e614bc6603965917f52884d2871f0a537d28b50
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-27 14:26:24 +0300

    Lifetime hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ahgular-learning';
+export class AppComponent implements OnInit, OnDestroy {
+    intervalSub: any;
+
+    ngOnInit(): void {
+        this.intervalSub = setInterval(() => {
+            console.log('Hello from hell!');
+        }, 1000);
+    }
+
+    ngOnDestroy(): void {
+        if (this.intervalSub) {
+            clearInterval(this.intervalSub);
+        }
+    }
 }
