6047dbb91b15af90941afd5ed6f1b4f3b0705e5a
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-28 17:04:44 +0300

    Attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,28 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
     itemImageUrl = 'http://localhost:3000/public/img/0015/15/e6f96024d40a3.jpeg';
-    isSpecial = true;
-    currentClasses = {};
-    currentStyle = {};
-    name = 'John Doe';
 
     ngOnInit() {
-        this.setCurrentClasses();
-        this.setCurrentStyle();
-    }
 
-    setCurrentClasses() {
-        this.currentClasses = {
-            saveable: true,
-            modified: false,
-            special: true
-        };
-    }
-
-    setCurrentStyle() {
-        this.currentStyle = {
-            fontStyle: 'italic',
-            fontWeight: 'bold'
-        }
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
 import { GreetingsPipe } from './greetings.pipe';
 import { ItemComponent } from './item/item.component';
 import { AppSizerComponent } from './app-sizer/app-sizer.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ParentComponent,
-    ChildComponent,
-    GreetingsPipe,
-    ItemComponent,
-    AppSizerComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+        AppComponent,
+        ParentComponent,
+        ChildComponent,
+        GreetingsPipe,
+        ItemComponent,
+        AppSizerComponent,
+        HighlightDirective,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
