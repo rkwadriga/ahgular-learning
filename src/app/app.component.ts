707c0deda4263c2e1a1af418441d84aa7ab08fe7
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-28 16:49:33 +0300

    Directives (how to say the elements how thay should behaves)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     itemImageUrl = 'http://localhost:3000/public/img/0015/15/e6f96024d40a3.jpeg';
-    phoneNumber = '1111-22-33';
+    isSpecial = true;
+    currentClasses = {};
+    currentStyle = {};
+    name = 'John Doe';
 
-    onCall() {
-        console.log(this.phoneNumber);
+    ngOnInit() {
+        this.setCurrentClasses();
+        this.setCurrentStyle();
+    }
+
+    setCurrentClasses() {
+        this.currentClasses = {
+            saveable: true,
+            modified: false,
+            special: true
+        };
+    }
+
+    setCurrentStyle() {
+        this.currentStyle = {
+            fontStyle: 'italic',
+            fontWeight: 'bold'
+        }
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from "@angular/forms";
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from "@angular/forms";
+
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
