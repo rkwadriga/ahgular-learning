c1fa8ff72e40801e8a61043b891229d244826a7f
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-28 12:53:34 +0300

    Template statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Hello world!';
+    showText = false;
+
+    toggleText(event: any) {
+        this.showText = !this.showText
+        console.log(event);
+    }
 }
