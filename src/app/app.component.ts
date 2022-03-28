015f77766b6c565799e28f85d088a4f5ed6ab4d0
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-28 14:03:09 +0300

    Attribute, class and binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    onSale = true;
     itemImageUrl = 'http://localhost:3000/public/img/0015/15/e6f96024d40a3.jpeg'
 }
