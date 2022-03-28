e26e6cd3a4c92f58184c60f39c2030e8a1de497d
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-28 13:07:25 +0300

    Custom pipes (formatters)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    showText = false;
-
-    toggleText(event: any) {
-        this.showText = !this.showText
-        console.log(event);
-    }
+    todayDate = new Date();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { GreetingsPipe } from './greetings.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    GreetingsPipe,
   ],
   imports: [
     BrowserModule,
