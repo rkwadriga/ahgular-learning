d0be27c85bf20e0847e09697c01fb5070a0b4e09
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-29 14:20:23 +0300

    Form validation (using FormGroup validation)

--- src/app/child-a/child-a.component.ts
+++ src/app/child-a/child-a.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-child-a',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildAComponent implements OnInit {
     nameInput = '';
+    validationForm = new FormGroup({
+        name: new FormControl(this.nameInput, [
+            Validators.required,
+            Validators.minLength(4)
+        ])
+    });
 
     constructor() { }
 
     ngOnInit(): void {
+    }
+
+    get name() {
+        return this.validationForm.get('name');
     }
 }
