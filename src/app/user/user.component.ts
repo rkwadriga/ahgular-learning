9aba3eb148ce79a59ad76a64ba7e2bb32efeaeb5
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-29 11:06:30 +0300

    Dependency injection

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Renderer2, ElementRef} from '@angular/core';
+import {LogService} from "../log.service";
 
 interface User {
     name: string,
@@ -13,8 +14,14 @@ interface User {
 export class UserComponent implements OnInit {
     @Input() users: User[] = [];
 
-    constructor() { }
+    constructor(
+        private readonly logger: LogService,
+        private readonly renderer: Renderer2,
+        private readonly host: ElementRef
+    ) { }
 
     ngOnInit(): void {
+        this.logger.log('Hello from user component');
+        this.renderer.setStyle(this.host.nativeElement, 'color', 'blue');
     }
 }
