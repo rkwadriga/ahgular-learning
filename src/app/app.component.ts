52d8fb7948eb38c592b6227eb6012460142f65e6
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-29 12:29:38 +0300

    Template-driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Pet} from './pet.model';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    species = ['dog', 'cat', 'woman'];
+    model = new Pet(1, 'Goldie', this.species[2]);
+    submitted = false;
+
     ngOnInit() {
 
+    }
+
+    onSubmit() {
+        this.submitted = true;
+        console.log(this.model);
     }
 }
